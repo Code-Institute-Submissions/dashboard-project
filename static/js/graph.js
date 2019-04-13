eeb0420bd10d74de109230a085a8da82f1cf7091
Warren Howes <warren.howes@hotmail.co.uk>
2019-04-13 16:45:58 +0000

    added a bar chart to show the average age that watches the shows

--- static/js/graph.js
+++ static/js/graph.js
@@ -5,8 +5,16 @@ queue()
 function makeGraphs(error, dragonballzData) {
     var ndx = crossfilter(dragonballzData);
     
+    dragonballzData.forEach(function(d){
+        d.age = parseInt(d.age);
+    })
+    
+   
+    
     show_graph_selector(ndx);
     show_gender_graph(ndx);
+    show_average_age(ndx);
+    show_show_distribution(ndx)
     
     dc.renderAll();
 }
@@ -38,3 +46,65 @@ function show_gender_graph(ndx) {
         .xAxisLabel("Gender")
         .yAxis().ticks(20);
 }
+
+
+
+function show_average_age(ndx) {
+    var dim = ndx.dimension(dc.pluck('sex'));
+    
+    function add_item(p, v) {
+        p.count++;
+        p.total += v.age;
+        p.average = p.total / p.count;
+        return p;
+    }
+
+    function remove_item(p, v) {
+        p.count--;
+        if(p.count == 0) {
+            p.total = 0;
+            p.average = 0;
+        } else {
+            p.total -= v.age;
+            p.average = p.total / p.count;
+        }
+        return p;
+    }
+    
+    function initialise() {
+        return {count: 0, total: 0, average: 0};
+    }
+
+    var averageageByGender = dim.group().reduce(add_item, remove_item, initialise);
+
+    dc.barChart("#average-age")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(averageageByGender)
+        .valueAccessor(function(d){
+            return d.value.average.toFixed(2);
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(4);   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
